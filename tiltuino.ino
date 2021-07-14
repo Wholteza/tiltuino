0d0dbee34c5cb4284af5e59b549581cd8728e988
Zackarias Montell <zackarias@montell.se>
2021-07-14 19:09:31 +0200

    Fixes for issues discovered when testing our irl

--- tiltuino.ino
+++ tiltuino.ino
@@ -4,23 +4,24 @@
 
 char asciiBanner[] = " ▄▄▄▄▄▄▄ ▄▄▄ ▄▄▄     ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄ \n█       █   █   █   █       █  █ █  █   █  █  █ █       █\n█▄     ▄█   █   █   █▄     ▄█  █ █  █   █   █▄█ █   ▄   █\n  █   █ █   █   █     █   █ █  █▄█  █   █       █  █ █  █\n  █   █ █   █   █▄▄▄  █   █ █       █   █  ▄    █  █▄█  █\n  █   █ █   █       █ █   █ █       █   █ █ █   █       █\n  █▄▄▄█ █▄▄▄█▄▄▄▄▄▄▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄█▄█  █▄▄█▄▄▄▄▄▄▄█";
 
-char wifiSsid[] = "SSID";
-char wifiPassword[] = "PASSWORD";
+char wifiSsid[] = "abc";
+char wifiPassword[] = "abc";
 
-char tiltBluetoothAddress[] = "AA:BB:CC:DD:EE:FF";
+char tiltBluetoothAddress[] = "00:00:00:00:00:00";
 
 String customDeviceName = "Tiltuino";
+char hostname[] = "tiltuino";
 char brewfatherRootAddress[] = "log.brewfather.net";
-char brewfatherCustomStreamPath[] = "/stream?id=yourpersonalid";
-long sendInterval = 900000;
+char brewfatherCustomStreamPath[] = "/stream?id=abc";
+long sendInterval = 10000;
 bool useCelsius = true;
-bool useCustomDns = true;
+bool useCustomDns = false;
 IPAddress customDns(1, 1, 1, 1);
 
 
 bool debugMode = true;
+bool sendDataToBrewfather = true;
 
-int wifiStatus = WL_IDLE_STATUS;
 WiFiClient wifiClient;
 HttpClient httpClient = HttpClient(wifiClient, brewfatherRootAddress, 80);
 
@@ -37,66 +38,88 @@ void setup() {
 
 bool startBluetooth() {
   if (!BLE.begin()) {
-    Serial.println("starting BLE failed!");
+    if (debugMode) {
+      Serial.println("starting BLE failed!");
+    }
     return false;
   }
-  Serial.println("===BLUETOOTH START=======================================");
-  BLE.scan();
-  Serial.println("Started scanning..");
+  if (debugMode) {
+    Serial.println("===BLUETOOTH START=======================================");
+  }
   return true;
 }
 
 bool stopBluetooth() {
   BLE.end();
-  Serial.println("===BLUETOOTH STOP========================================");
+  if (debugMode) {
+    Serial.println("===BLUETOOTH STOP========================================");
+  }
 }
 
-void getNextValueFromTiltHydrometer(float reading[]) {
-  BLEDevice peripheral = BLE.available();
-  if (peripheral && peripheral.address() == tiltBluetoothAddress && peripheral.hasManufacturerData()) {
-    if (debugMode) {
-      Serial.println("===READING START=========================================");
-
-      Serial.print("Address: ");
-      Serial.println(peripheral.address());
-
-      Serial.print("RSSI: ");
-      Serial.println(peripheral.rssi());
-    }
-    String manufacturerData = peripheral.manufacturerData();
-    if (debugMode) {
-      Serial.print("Manufacturer data: ");
-      Serial.println(manufacturerData);
-    }
-
-    String temperatureHex = manufacturerData.substring(40, 44);
-    String gravityHex = manufacturerData.substring(44, 48);
-
-    char tempHexAsChar[5];
-    temperatureHex.toCharArray(tempHexAsChar, 5);
-    float tempFarenheit = strtol(tempHexAsChar, NULL, 16);
-    float tempCelsius = (tempFarenheit - 32) * .5556;
-    if (debugMode) {
-      Serial.print("Temp: ");
-      Serial.println(useCelsius ? tempCelsius : tempFarenheit);
-    }
-
-    char gravityAsChar[5];
-    gravityHex.toCharArray(gravityAsChar, 5);
-    long gravityAsDecimalTimesThousand = strtol(gravityAsChar, NULL, 16);
-    float gravity = gravityAsDecimalTimesThousand / 1000.0f;
-    if (debugMode) {
-      Serial.print("Gravity: ");
-      Serial.println(gravity, 3);
-      Serial.println("===READING END===========================================");
-    }
-
-    reading[0] = useCelsius ? tempCelsius : tempFarenheit;
-    reading[1] = gravity;
-    return;
+int getNextValueFromTiltHydrometer(float reading[]) {
+  int scanningSuccessful = BLE.scan();
+  if (debugMode) {
+    Serial.println("Scanning " + scanningSuccessful ? "successful" : "failed");
   }
-  reading[0] = 0;
-  reading[1] = 0;
+  if (scanningSuccessful = 0){
+    // Reset reading and return
+    reading[0] = 0;
+    reading[1] = 0;
+    return 0;
+  }
+  BLEDevice peripheral = BLE.available();
+  if(debugMode){
+      Serial.println("---------");
+      Serial.println("Found device");
+      Serial.println("Address " + peripheral.address());
+      Serial.println("HasManufacturerData " + peripheral.hasManufacturerData() ? "yes" : "no");
+    }
+  if (peripheral.address() != tiltBluetoothAddress || peripheral.hasManufacturerData() == 0){
+    // Reset reading and return
+    reading[0] = 0;
+    reading[1] = 0;
+    return 0;
+  }
+  if (debugMode) {
+    Serial.println("===READING START=========================================");
+
+    Serial.print("Address: ");
+    Serial.println(peripheral.address());
+
+    Serial.print("RSSI: ");
+    Serial.println(peripheral.rssi());
+  }
+  String manufacturerData = peripheral.manufacturerData();
+  if (debugMode) {
+    Serial.print("Manufacturer data: ");
+    Serial.println(manufacturerData);
+  }
+
+  String temperatureHex = manufacturerData.substring(40, 44);
+  String gravityHex = manufacturerData.substring(44, 48);
+
+  char tempHexAsChar[5];
+  temperatureHex.toCharArray(tempHexAsChar, 5);
+  float tempFarenheit = strtol(tempHexAsChar, NULL, 16);
+  float tempCelsius = (tempFarenheit - 32) * .5556;
+  if (debugMode) {
+    Serial.print("Temp: ");
+    Serial.println(useCelsius ? tempCelsius : tempFarenheit);
+  }
+
+  char gravityAsChar[5];
+  gravityHex.toCharArray(gravityAsChar, 5);
+  long gravityAsDecimalTimesThousand = strtol(gravityAsChar, NULL, 16);
+  float gravity = gravityAsDecimalTimesThousand / 1000.0f;
+  if (debugMode) {
+    Serial.print("Gravity: ");
+    Serial.println(gravity, 3);
+    Serial.println("===READING END===========================================");
+  }
+
+  reading[0] = useCelsius ? tempCelsius : tempFarenheit;
+  reading[1] = gravity;
+  return 1;
 }
 
 bool startWifi() {
@@ -125,9 +148,9 @@ void stopWifi() {
 
 void sendReadingToBrewfather(float reading[]) {
   if (WiFi.status() == WL_CONNECTED) {
-    String ipAddress = IpAddressToString(WiFi.localIP());
+    httpClient = HttpClient(wifiClient, brewfatherRootAddress, 80);
     String requestBody = "{\n    \"name\": \"" + customDeviceName + "\",\n    \"temp\": \"" + String(reading[0]) + "\",\n    \"temp_unit\": \"" + (useCelsius ? "C" : "F") + "\",\n    \"gravity\": \"" + String(reading[1]) + "\",\n    \"gravity_unit\": \"G\"\n}";
-    String request = "POST " + String(brewfatherCustomStreamPath) + " HTTP/1.1\nContent-Type: application/json\nAccept: */*\nHost: " + ipAddress + "\nAccept-Encoding: gzip, deflate, br\nConnection: keep-alive\nContent-Length: " + requestBody.length() + "\n\n" + requestBody;
+    String request = "POST " + String(brewfatherCustomStreamPath) + " HTTP/1.1\nContent-Type: application/json\nAccept: */*\nHost: " + brewfatherRootAddress + "\nAccept-Encoding: gzip, deflate, br\nConnection: keep-alive\nContent-Length: " + requestBody.length() + "\n\n" + requestBody;
     if (debugMode) {
       Serial.print("Attempting to log to: ");
       Serial.print(brewfatherRootAddress);
@@ -136,9 +159,12 @@ void sendReadingToBrewfather(float reading[]) {
       Serial.println(request);
       Serial.println("===REQUEST END===========================================");
     }
-    if (debugMode) {
-      httpClient.post(brewfatherCustomStreamPath, "application/json", requestBody);
-      int statusCode = httpClient.responseStatusCode();
+    int statusCode = 0;
+    if (sendDataToBrewfather) {
+      httpClient.post(brewfatherCustomStreamPath, "application/json", requestBody); 
+      statusCode = httpClient.responseStatusCode();
+    }
+    if (debugMode && sendDataToBrewfather) {
       Serial.println("===RESPONSE START========================================");
       Serial.print("Status code: ");
       Serial.println(statusCode);
@@ -151,26 +177,30 @@ void sendReadingToBrewfather(float reading[]) {
 }
 
 void printWifiwifiStatus() {
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  IPAddress ip = WiFi.localIP(); // Device IP address
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+  if (debugMode) {
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+  
+    IPAddress ip = WiFi.localIP(); // Device IP address
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+  }
 }
 
 void connectToAP() {
-  while (wifiStatus != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     if (debugMode) {
       Serial.print("Attempting to connect to wifi network: ");
       Serial.print(wifiSsid);
-      Serial.print("...");
+      Serial.println("...");
     }
-    wifiStatus = WiFi.begin(wifiSsid, wifiPassword);
-
+    WiFi.setHostname(hostname);
+    WiFi.begin(wifiSsid, wifiPassword);
   }
   delay(1000);
-  Serial.println("Success!");
+  if (debugMode) {
+    Serial.println("Success!");
+  }
   if (useCustomDns) {
     WiFi.setDNS(customDns);
     if (debugMode) {
@@ -190,13 +220,17 @@ String IpAddressToString(const IPAddress& ipAddress)
 }
 
 void loop() {
-  if (debugMode){
+  if (debugMode) {
     Serial.println("===FETCHING NEW VALUE START================================");
   }
   float reading[2] = {0, 0};
   if (startBluetooth()) {
-    while (reading[0] == 0 || reading[1] == 0) {
-      getNextValueFromTiltHydrometer(reading);
+    int successfulReading = 0;
+    while (successfulReading == 0) {
+      successfulReading = getNextValueFromTiltHydrometer(reading);
+      if (successfulReading == 0){
+        delay(1000);
+      }
     }
     stopBluetooth();
   }
@@ -204,7 +238,7 @@ void loop() {
     sendReadingToBrewfather(reading);
     stopWifi();
   }
-    if (debugMode){
+  if (debugMode) {
     Serial.println("===FETCHING NEW VALUE END================================");
     Serial.println(String("===WAITING ") + String(sendInterval) + String("MS======================================"));
   }
